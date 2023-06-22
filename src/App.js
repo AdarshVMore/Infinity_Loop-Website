e8b3175a504b03854a5ddcf8e78e6a11d5d0460a
AdarshVMore <skullcrushermore@gmail.com>
2023-06-22 14:53:17 +0530

    integrated Rainbow kit and solved some small bugs

--- src/App.js
+++ src/App.js
@@ -6,18 +6,57 @@ import Dex from "./pages/dex/Dex";
 import CommingSoon from "./pages/comming_soon/CommingSoon";
 import TokenSale from "./pages/tokensale/TokenSale";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: "30980df3d204e25c5ae7cfe90918d5ef",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Landing />} />
-          <Route path="/dex*" element={<Dex />} />
-          <Route path="/dexx" element={<CommingSoon />} />
-          <Route path="/token-sale" element={<TokenSale />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={darkTheme({
+          accentColor: "#0077ff",
+          accentColorForeground: "white",
+          borderRadius: "large",
+        })}
+      >
+        <div className="App">
+          <BrowserRouter>
+            <Routes>
+              <Route path="" element={<Landing />} />
+              <Route path="/dex*" element={<Dex />} />
+              <Route path="/dexx" element={<CommingSoon />} />
+              <Route path="/token-sale" element={<TokenSale />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
--- src/components/tokensale/TokenSale.jsx
+++ src/components/tokensale/TokenSale.jsx
@@ -6,46 +6,48 @@ function TokenSale() {
   const [popup, setPopup] = useState(false);
   return (
     <div className="token-sale">
-      <div className={popup ? "floating-container" : "hide"}>
-        <div className="top-float">
-          <IoIosArrowBack
-            size={24}
-            onClick={() => {
-              setPopup(false);
-            }}
-          />
-          <h3>Exchange</h3>
-          <div></div>
-        </div>
-        <div className="exchange">
+      <div className={` ${popup ? "popup-container" : "hide"}`}>
+        <div className="floating-container">
           <div className="top-float">
-            <div className="select">
-              <p>From</p>
-              <select name="" id=""></select>
+            <IoIosArrowBack
+              size={24}
+              onClick={() => {
+                setPopup(false);
+              }}
+            />
+            <h3>Exchange</h3>
+            <div></div>
+          </div>
+          <div className="exchange">
+            <div className="top-float">
+              <div className="select">
+                <p>From</p>
+                <select name="" id=""></select>
+              </div>
+              <div className="input">
+                <div className="value">
+                  <input type="text" placeholder="0" />
+                  <p>MAX</p>
+                </div>
+              </div>
             </div>
-            <div className="input">
-              <div className="value">
-                <input type="text" placeholder="0" />
-                <p>MAX</p>
+            <div className="bottom-float">
+              <div className="select">
+                <p>To</p>
+                {/* <select name="" id=""></select> */}
+                <p>LOI</p>
+              </div>
+              <div className="input">
+                <div className="value">
+                  <p>0</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="bottom-float">
-            <div className="select">
-              <p>To</p>
-              {/* <select name="" id=""></select> */}
-              <p>LOI</p>
-            </div>
-            <div className="input">
-              <div className="value">
-                <p>0</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="price-loi">Price 1 LOI - $0.0080</div>
+          <div className="price-loi">Price 1 LOI - $0.0080</div>
 
-        <button>Buy Now</button>
+          <button>Buy Now</button>
+        </div>
       </div>
       <div className="container1">
         <h1 className=" days-one">LOI Token Sale</h1>
@@ -147,28 +149,6 @@ function TokenSale() {
         <div className="left">
           <div className="info"></div>
         </div>
-        {/* <div className="right">
-          <div className="feature">
-            <div className="heading">Loss-proof DEX integration</div>
-            <div className="feature-info">
-              Loop Of Infinity addresses investment risk by integrating a
-              loss-proof DEX that covers up to 60% of losses. This boosts player
-              confidence, encourages broader participation, and ensures peace of
-              mind in the gaming platform
-            </div>
-            <button>Read Documentation</button>
-          </div>
-          <div className="feature">
-            <div className="heading">Layer-2 scaling solution</div>
-            <div className="feature-info">
-              Loop Network's layer-2 scaling solution boosts scalability for
-              blockchain gaming platforms. With increased transaction processing
-              capacity, Loop Of Infinity provides users a seamless and expedited
-              gaming experience.
-            </div>
-            <button>Read Documentation</button>
-          </div>
-        </div> */}
       </div>
     </div>
   );
